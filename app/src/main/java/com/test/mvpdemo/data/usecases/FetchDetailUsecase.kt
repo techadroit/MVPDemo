6f273a17bf9c5c102b8e95f77db12f397697e5fa
saghayam1989 <techadroit89@gmail.com>
2018-10-14 20:48:33 +0530

    removed null data from the api

--- app/src/main/java/com/test/mvpdemo/data/usecases/FetchDetailUsecase.kt
+++ app/src/main/java/com/test/mvpdemo/data/usecases/FetchDetailUsecase.kt
@@ -2,10 +2,13 @@ package com.test.mvpdemo.data.usecases
 
 import com.test.mvpdemo.data.network.ApiService
 import com.test.mvpdemo.data.response.DetailResponse
+import com.test.mvpdemo.util.isNull
 import io.reactivex.Observable
 
 class FetchDetailUsecase constructor(var apiService: ApiService) : Interactor<DetailResponse> {
     override fun execute(): Observable<DetailResponse> {
-        return apiService.getData()
+        return apiService.getData().filter {
+            !isNull(it.title)
+        }
     }
 }
